27f002fb813b6632545664ca3a5d8ee21f2189a8
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-07-19 22:56:13 -0300

    fix: icone no nav / imagem no carossel

--- src/app/pages/landing-page/menu-nav/menu-nav.ts
+++ src/app/pages/landing-page/menu-nav/menu-nav.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu-nav',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './menu-nav.html',
   styleUrl: './menu-nav.scss'
 })
