415ba7665488a88bd45658323dad8aeb5936fbe3
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-07-24 23:24:51 -0300

    feat: adiciona possibilidade de encerrar a sessao

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -10,5 +12,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   styleUrl: './dashboard-page.scss'
 })
 export class DashboardPage {
+  private router = inject(Router);
+  private snackBar = inject(MatSnackBar);
 
+  logout() {
+    localStorage.removeItem('auth_token');
+    this.snackBar.open('Sessão encerada com sucesso!', 'Fechar', { duration: 3000 });
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
