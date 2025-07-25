ecf850902e06ba2a19c173505aff8e3c44fc7d35
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-07-25 13:27:31 -0300

    fix: proteção de rota para Dashboard funcionando
    Não estava chamando a função para limpar o 'auth_token'

--- src/app/pages/dashboard-page/dashboard-page.ts
+++ src/app/pages/dashboard-page/dashboard-page.ts
@@ -21,7 +21,7 @@ export class DashboardPage {
   readonly payload = this.authService.getDecodedToken();
 
   logout() {
-    this.authService.logout;
+    this.authService.logout();
     this.snackBar.open('Sessão encerada com sucesso!', 'Fechar', { duration: 3000 });
     this.router.navigate(['/login']);
   }
