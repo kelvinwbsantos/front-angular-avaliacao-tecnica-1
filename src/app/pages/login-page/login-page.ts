4a584724627ca8f2c2d49c2f0cbc9dd949fa0abc
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-08-01 14:10:14 -0300

    fix: redireciona usuario autenticado de login para dashboard

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LoginForm } from "./login-form/login-form";
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -8,5 +10,13 @@ import { LoginForm } from "./login-form/login-form";
   styleUrl: './login-page.scss'
 })
 export class LoginPage {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  ngOnInit() {
+    if (this.authService.isAuthenticated()) {
+      this.router.navigate(['/dashboard']);
+    }
+  }
 
 }
