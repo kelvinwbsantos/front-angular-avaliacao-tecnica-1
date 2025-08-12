e64239014aeea6233d3cd65a41f5faa421fd10a3
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-08-12 13:25:34 -0300

    refactor: mudar cor da navbar

--- src/app/pages/landing-page/menu-nav/menu-nav.ts
+++ src/app/pages/landing-page/menu-nav/menu-nav.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-menu-nav',
-  imports: [MatButtonModule, MatIconModule, MatSlideToggleModule],
+  imports: [MatButtonModule, MatIconModule, MatSlideToggleModule, MatDividerModule, MatListModule],
   templateUrl: './menu-nav.html',
   styleUrl: './menu-nav.scss'
 })
