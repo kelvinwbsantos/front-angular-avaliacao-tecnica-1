f7f27140a50b30f5d0d02e97389da94ecc450fb0
kelvinwbsantos <145406898+kelvinwbsantos@users.noreply.github.com>
2025-08-12 21:45:00 -0300

    refactor: css da rota admin

--- src/app/pages/dashboard-page/admin/admin.ts
+++ src/app/pages/dashboard-page/admin/admin.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ListUsers } from "./list-users/list-users";
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-admin',
-  imports: [ListUsers],
+  imports: [ListUsers, MatTabsModule],
   templateUrl: './admin.html',
   styleUrl: './admin.scss'
 })
